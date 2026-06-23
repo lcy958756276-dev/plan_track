4ec0cd7374b64dfb66798fb5f5c6e6434242308c
lcy958756276-dev <lcy958756276@gmail.com>
2026-06-23 22:28:45 +0800

    update 2026-06-23 22:28:45

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/lcy/robot_graduation/devel:$CMAKE_PREFIX_PATH"
+export PATH='/opt/ros/noetic/bin:/home/lcy/node-v24.16.0-linux-arm64/bin:/home/lcy/.config/Code/User/globalStorage/github.copilot-chat/debugCommand:/home/lcy/.config/Code/User/globalStorage/github.copilot-chat/copilotCli:/home/lcy/node-v24.16.0-linux-arm64/bin:/home/lcy/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/lcy/.vscode/extensions/ms-python.debugpy-2026.6.0-linux-arm64/bundled/scripts/noConfigScripts'
+export PWD='/home/lcy/robot_graduation/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/lcy/robot_graduation/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/lcy/robot_graduation/src:$ROS_PACKAGE_PATH"
---
+++ build/map_saver/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lcy/robot_graduation/src/map_saver
+# Build directory: /home/lcy/robot_graduation/build/map_saver
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
